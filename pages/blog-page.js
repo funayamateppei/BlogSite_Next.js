ea6071214b2c0a43bca2e85a5a55645ca0e00230
funayamateppei <t.funayama0217@gmail.com>
2023-02-24 16:06:48 +0900

    外部APIで取得した値を使って静的サイトを生成(SSG)を実装 ブログ一覧ページを実装

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,14 +1,26 @@
-import React from 'react'
-import Layout from '../components/Layout'
+import React from "react";
+import { GetStaticProps } from "next";
+import Layout from "../components/Layout";
+import Post from "../components/Post";
+import { getAllPostsData } from "../lib/posts";
 
-const Blog = () => {
+const Blog = ({ posts }) => {
   return (
     <div>
       <Layout title="Blog">
-        <p className="text-4xl">Blog Page</p>
+        <ul className="m-10">
+          {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+        </ul>
       </Layout>
     </div>
   );
-}
+};
 
-export default Blog
+export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
